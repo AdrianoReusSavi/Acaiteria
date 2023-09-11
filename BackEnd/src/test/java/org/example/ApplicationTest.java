52d8cf5258d3e847e557291017cbedfaeb7c673c
Adriano <adrianorsavi@gmail.com>
2023-09-11 20:02:31 -0300

    Criação da aplicação BackEnd

---
+++ BackEnd/src/test/java/org/example/ApplicationTest.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ApplicationTest {
+
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
