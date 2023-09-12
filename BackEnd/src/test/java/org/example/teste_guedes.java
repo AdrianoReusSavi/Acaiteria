a113c0c6e49f377f3cb4990d2b3e0e601901bc7a
Pedroguedez <pedroguedes158@icloud.com>
2023-09-11 21:27:59 -0300

    feat/teste:Primeiro teste

---
+++ BackEnd/src/test/java/org/example/teste_guedes.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ApplicationGuedesTest {
+
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
