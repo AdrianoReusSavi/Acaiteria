8d8d6fbbabb6afc7770ad8f057d6d6b3287248d7
Gabriel Della Giustina <100289323+GabrielSouzaDG@users.noreply.github.com>
2023-09-18 19:11:57 -0300

    classes básicas do projeto
    esqueleto de classes do projeto, sujeito a mudanças.

---
+++ BackEnd/src/main/java/org/example/model/EntityId.java
@@ -0,0 +1,13 @@
+package org.example.model;
+
+public class EntityId {
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
---
+++ BackEnd/src/main/java/org/example/model/Usuario.java
@@ -0,0 +1,8 @@
+package org.example.model;
+
+public abstract class Usuario extends EntityId {
+    private String nome;
+    private String senha;
+    private String email;
+
+}
