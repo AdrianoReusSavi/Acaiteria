d3ab91bdf5cd437f10aa44b77939f284621f8ffe
adriano <adrianorsavi@gmail.com>
2023-11-25 20:40:54 -0300

    Comunicação Back Front tela Menu

--- FrontEnd/point/src/app/page.tsx
+++ FrontEnd/point/src/app/page.tsx
@@ -1,84 +1,112 @@
-'use client'
+﻿'use client'
 import React, { useState, useEffect } from "react";
 import CartModal from "@/components/ModalCar/ModalCar";
 import Filtro from "@/components/Navbartb/Filtro";
 import { styled } from "styled-components";
 
 interface ProductProps {
-  name: string;
-  vlrfl: number;
-  image: string;
-  checked: boolean;
-  price: number;
-  descricao: string;
+    id: number;
+    descricao: string;
+    precoVenda: number;
+    imagem: string;
+    ativo: boolean;
+    filtro: string;
 }
 
 interface CartItem {
-  name: string;
-  price: number;
-  quantity: number;
+    id: number;
+    descricao: string;
+    precoVenda: number;
+    quantidade: number;
 }
 
 export default function Menu() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [activeProducts, setActiveProducts] = useState<ProductProps[]>([]);
-  const [selectedProduct, setSelectedProduct] = useState<ProductProps | null>(null);
-  const [productClickCount, setProductClickCount] = useState<{ [key: string]: number }>({});
-  const [deleteProduct, setDeleteProduct] = useState<ProductProps | null>(null);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [activeProducts, setActiveProducts] = useState<ProductProps[]>([]);
+    const [selectedProducts, setSelectedProducts] = useState<ProductProps[] | null>(null);
+    const [productClickCount, setProductClickCount] = useState<{ [key: number]: number }>({});
+    const [deleteProduct, setDeleteProduct] = useState<ProductProps | null>(null);
 
 
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
+    const openModal = () => {
+        if (selectedProducts && selectedProducts.length > 0) {
+            setIsModalOpen(true);
+        }
+    };
 
-  const selectProduct = (product: ProductProps) => {
-    setSelectedProduct({ ...product, price: product.vlrfl });
-    handleProductClick(product.name);
-  };
+    const selectProduct = (product: ProductProps) => {
+        // Verificar se o produto já está na lista
+        const isProductAlreadySelected = selectedProducts?.some((p) => p.id === product.id);
 
-  const handleProductClick = (productName: string) => {
-    setProductClickCount((prevCounts) => ({
-      ...prevCounts,
-      [productName]: (prevCounts[productName] || 0) + 1,
-    }));
-  };
+        if (!isProductAlreadySelected) {
+            const updatedSelectedProducts = selectedProducts ? [...selectedProducts, product] : [product];
+            setSelectedProducts(updatedSelectedProducts);
 
-  useEffect(() => {
-    const savedData = localStorage.getItem("stockData");
-    if (savedData) {
-      const parsedData: ProductProps[] = JSON.parse(savedData);
-      const filteredProducts = parsedData.filter((product) => product.checked);
-      setActiveProducts(filteredProducts);
-    }
-  }, []);
+            // Iniciar a quantidade com 1 ao adicionar um novo produto
+            setProductClickCount((prevCounts) => ({
+                ...prevCounts,
+                [product.id]: 1,
+            }));
+        } else {
+            // Se o produto já estiver na lista, apenas incrementar a quantidade
+            setProductClickCount((prevCounts) => ({
+                ...prevCounts,
+                [product.id]: (prevCounts[product.id] || 0) + 1,
+            }));
+        }
+    };
 
-  const convertToCartItems = (clickCount: { [key: string]: number }, selectedProduct: ProductProps | null): CartItem[] => {
-    if (selectedProduct) {
-      const cartItems: CartItem[] = Object.entries(clickCount).map(([name, quantity]) => ({
-        name,
-        price: selectedProduct.price,
-        quantity,
-      }));
-      return cartItems;
-    } else {
-      return [];
-    }
-  };
-  //const cartItems = convertToCartItems(clickCount, selectedProduct);
-  const getCategoryColor = (descricao: string) => {
-    switch (descricao.toLowerCase()) {
-      case 'para':
-        return 'bg-red-500';
-      case 'bebidas':
-        return 'bg-blue-500';
-      case 'acai':
-        return 'bg-purple-500';
-      default:
-        return 'bg-gray-500';
-    }
-  };
+    useEffect(() => {
+        const savedData = localStorage.getItem("stockData");
+        if (savedData) {
+            const parsedData: ProductProps[] = JSON.parse(savedData);
+            const filteredProducts = parsedData.filter((product) => product.ativo);
+            setActiveProducts(filteredProducts);
+        }
+        //        fetch('http://localhost:8080/api/item')
+        //            .then((response) => response.json())
+        //            .then((data) => {
+        //                setActiveProducts(data.content);
+        //            })
+        //             .catch((error) => {
+        //                console.error('Erro ao buscar os produtos:', error);
+        //            });
+    }, []);
 
-  const CartCount = styled.span`
+    const convertToCartItems = (clickCount: { [key: number]: number }, selectedProducts: ProductProps[] | null): CartItem[] => {
+        if (selectedProducts) {
+            const cartItems: CartItem[] = selectedProducts.flatMap((product) => {
+                const quantidade = clickCount[product.id] || 0;
+
+                return {
+                    id: product.id,
+                    descricao: product.descricao,
+                    precoVenda: product.precoVenda,
+                    quantidade: quantidade,
+                };
+            });
+
+            return cartItems;
+        } else {
+            return [];
+        }
+    };
+
+    //const cartItems = convertToCartItems(clickCount, selectedProduct);
+    const getCategoryColor = (filtro: string) => {
+        switch (filtro.toLowerCase()) {
+            case 'para':
+                return 'bg-red-500';
+            case 'bebidas':
+                return 'bg-blue-500';
+            case 'acai':
+                return 'bg-purple-500';
+            default:
+                return 'bg-gray-500';
+        }
+    };
+
+    const CartCount = styled.span`
     width: 17px;
     height: 17px;
     background-color: var(--count-color);
@@ -88,69 +116,92 @@ export default function Menu() {
     left:-10px;
     top:50%;
   `
-  const Container = styled.div`
+    const Container = styled.div`
   position: relative;
   `
 
-  const removeFromCart = (productName: string) => {
-    setProductClickCount((prevCounts) => {
-      const updatedCounts = { ...prevCounts };
-      delete updatedCounts[productName];
-      return updatedCounts;
-    });
-  };
-  const handleClearCart = () => {
-    setProductClickCount({});
-  };
+    const handleClearCart = () => {
+        setSelectedProducts([]);
+        setProductClickCount({});
+        setIsModalOpen(false);
+    };
 
-  const handleCancelItemClick = (productName: string) => {
-    removeFromCart(productName); 
-  };
-  //console.log('produto selecionado :', quantity);
-  return (
-    <div className="p-9 w-full">
-      <Filtro />
-      <div className="flex flex-wrap py-7 gap-4">
-        {activeProducts.map((product, index) => (
-          <div
-            key={index}
-            className={`w-44 h-48 flex flex-col items-center justify-center p-15 hover:scale-105 ${getCategoryColor(product.descricao)}`}
-            onClick={() => selectProduct(product)}
-          >
+    const handleCancelItemClick = (id: number) => {
+        const newProducts = selectedProducts?.filter((product) => product.id !== id);
 
-            <img
-              src={product.image}
-              alt={product.name}
-              className="w-20 h-22 mb-2"
-            />
-            <h3 className="text-xl font-bold">{product.name}</h3>
-            <h3 className="text-xl">{product.vlrfl}</h3>
-          </div>
-        ))}
-      </div>
-      <button
-        className="fixed bottom-8 right-9 bg-purple-700 hover:bg-purple-600 py-2 px-2 rounded-full"
-        onClick={() => openModal()}
-      >
-        <Container>
-          {Object.keys(productClickCount).length > 0 && (
-            <CartCount>
-              {Object.values(productClickCount).reduce((total, count) => total + count, 0)}
-            </CartCount>
-          )}
-        </Container>
-        <img src="carrinho.png" alt="carrinho" />
-      </button>
-      {isModalOpen && selectedProduct ? (
-        <CartModal
-          isOpen={isModalOpen}
-          product={selectedProduct}
-          cartItems={convertToCartItems(productClickCount, selectedProduct)}
-          onCancelItemClick={handleCancelItemClick}  
-          onCancelAll={handleClearCart}
-          onClose={() => setIsModalOpen(false)}
-        />
-      ) : null}
-    </div>
-  );
+        setSelectedProducts(newProducts || []);
+
+        setProductClickCount((prevCounts) => {
+            const updatedCounts = { ...prevCounts };
+            delete updatedCounts[id];
+
+            // Verifica se ambas as listas estão vazias
+            if (Object.keys(updatedCounts).length === 0 && (!newProducts || newProducts.length === 0)) {
+                setIsModalOpen(false);
+            }
+
+            return updatedCounts;
+        });
+    };
+
+    const handleCartItemUpdate = (updatedCartItems: CartItem[]) => {
+        // Atualizar productClickCount com base nas quantidades dos itens atualizadas
+        const updatedClickCount: { [key: number]: number } = {};
+
+        updatedCartItems.forEach((item) => {
+            updatedClickCount[item.id] = item.quantidade;
+        });
+
+        setProductClickCount(updatedClickCount);
+    };
+
+    //console.log('produto selecionado :', quantidadeEstoque);
+    return (
+        <div className="p-9 w-full">
+            <Filtro />
+            <div className="flex flex-wrap py-7 gap-4">
+                {activeProducts.map((product, index) => (
+                    <div
+                        key={index}
+                        className={`w-44 h-48 flex flex-col items-center justify-center p-15 hover:scale-105 ${getCategoryColor(product.filtro)}`}
+                        onClick={() => selectProduct(product)}
+                    >
+
+                        <img
+                            src={product.imagem}
+                            alt={product.descricao}
+                            className="w-20 h-22 mb-2"
+                        />
+                        <h3 className="text-xl font-bold">{product.descricao}</h3>
+                        <h3 className="text-xl">{product.precoVenda}</h3>
+                    </div>
+                ))}
+            </div>
+            <button
+                className="fixed bottom-8 right-9 bg-purple-700 hover:bg-purple-600 py-2 px-2 rounded-full"
+                onClick={() => openModal()}
+            >
+                <Container>
+                    {Object.keys(productClickCount).length > 0 && (
+                        <CartCount>
+                            {Object.values(productClickCount).reduce((total, count) => total + count, 0)}
+                        </CartCount>
+                    )}
+                </Container>
+                <img src="carrinho.png" alt="carrinho" />
+            </button>
+            {isModalOpen && selectedProducts ? (
+                <CartModal
+                    isOpen={isModalOpen}
+                    products={selectedProducts}
+                    cartItems={convertToCartItems(productClickCount, selectedProducts)}
+                    onCancelItemClick={handleCancelItemClick}
+                    onCancelAll={handleClearCart}
+                    onClose={() => setIsModalOpen(false)}
+                    onPaymentSuccess={() => setSelectedProducts([])}
+                    onCartItemUpdate={handleCartItemUpdate}
+                />
+            ) : null}
+        </div>
+    );
 }
--- FrontEnd/point/src/components/ModalCar/ModalCar.tsx
+++ FrontEnd/point/src/components/ModalCar/ModalCar.tsx
@@ -1,136 +1,236 @@
 
 import React, { useState, useEffect } from 'react';
+import { debug } from 'util';
+import axios from 'axios';
 interface ProductProps {
-  name: string;
-  vlrfl: number;
-  price: number;
+    id: number;
+    descricao: string;
+    precoVenda: number;
 }
 
 interface CartItem {
-  name: string;
-  price: number;
-  quantity: number;
+    id: number;
+    descricao: string;
+    precoVenda: number;
+    quantidade: number;
 }
 
 interface CartModalProps {
-  isOpen: boolean;
-  product: ProductProps | null;
-  cartItems: CartItem[];
-  onClose: () => void;
-  onCancelItemClick: (productName: string) => void;
-  onCancelAll:()=>void;
+    isOpen: boolean;
+    products: ProductProps[] | null;
+    cartItems: CartItem[];
+    onClose: () => void;
+    onCancelItemClick: (id: number) => void;
+    onCancelAll: () => void;
+    onPaymentSuccess: () => void;
+    onCartItemUpdate: (updatedCartItems: CartItem[]) => void;
 }
 
-const CartModal: React.FC<CartModalProps> = ({ isOpen, product, cartItems, onClose, onCancelItemClick, onCancelAll }) => {
-  const [cartItem, setCartItems] = useState<CartItem[]>(cartItems);
-  const [deleteProduct, setDeleteProduct] = useState(false);
+const CartModal: React.FC<CartModalProps> = ({ isOpen, products, cartItems, onClose, onCancelItemClick, onCancelAll, onPaymentSuccess, onCartItemUpdate }) => {
+    const [cartItem, setCartItems] = useState<CartItem[]>(cartItems);
+    const [cliente, setCliente] = useState("");
+    const [desconto, setDesconto] = useState(0);
+    const [mensagensDeErro, setMensagensDeErro] = useState<string[]>([]);
 
-  useEffect(() => {
-    setCartItems(cartItems); // Update cartItem state when cartItems prop changes
-  }, [cartItems]);
+    useEffect(() => {
+        setCartItems(cartItems); // Update cartItem state when cartItems prop changes
+    }, [cartItems]);
 
-  const findCartItemIndex = (productName: string) => {
-    return cartItem.findIndex((item) => item.name === productName);
-  };
+    const findCartItemIndex = (productName: string) => {
+        return cartItem.findIndex((item) => item.descricao === productName);
+    };
 
-  
-  const decreaseQuantity = (item: CartItem) => {
-    const itemIndex = findCartItemIndex(item.name);
-    if (itemIndex !== -1) {
-      const updatedCartItems = [...cartItem];
-      updatedCartItems[itemIndex].quantity -= 1;
-      setCartItems(updatedCartItems);
+
+    const decreasequantidade = (item: CartItem) => {
+        const itemIndex = findCartItemIndex(item.descricao);
+        if (itemIndex !== -1) {
+            const updatedCartItems = [...cartItem];
+            updatedCartItems[itemIndex].quantidade -= 1;
+            setCartItems(updatedCartItems);
+            onCartItemUpdate(updatedCartItems);
+        }
+    };
+
+    const increasequantidade = (item: CartItem) => {
+        const itemIndex = findCartItemIndex(item.descricao);
+        if (itemIndex !== -1) {
+            const updatedCartItems = [...cartItem];
+            updatedCartItems[itemIndex].quantidade += 1;
+            setCartItems(updatedCartItems);
+            onCartItemUpdate(updatedCartItems);
+        }
+    };
+
+    const handleDeleteClick = (itemName: string) => {
+        const updatedCartItems = cartItem.filter((item) => item.descricao !== itemName);
+        setCartItems(updatedCartItems);
+        onCartItemUpdate(updatedCartItems);
+    };
+
+    const enviarPedido = () => {
+        const valorTotal = cartItem.reduce((total, product) => {
+            return total + product.precoVenda * product.quantidade;
+        }, 0);
+        const descontoEmReais = (desconto / 100) * valorTotal;
+        const valorTotalComDesconto = valorTotal - descontoEmReais;
+        const pedido = {
+            dataHora: new Date().toISOString(),
+            valorTotal: valorTotalComDesconto,
+            status: "FECHADO",
+            desconto: desconto,
+            cliente: cliente,
+            pedidoItens: cartItem.map((product) => ({
+                item: { id: product.id },
+                descricaoItem: product.descricao,
+                quantidade: product.quantidade,
+                valorVenda: product.precoVenda * product.quantidade,
+            })),
+        };
+
+        axios
+            .post('http://localhost:8080/api/pedido', pedido)
+            .then((response) => {
+                if (response.status === 201) {
+                    onClose();
+                    onCancelAll();
+                    resetInputs();
+                    onPaymentSuccess()
+                } else if (response.data && response.data.erro) {
+                    setMensagensDeErro([response.data.erro]);
+                }
+            })
+            .catch((error) => {
+                if (error.response && error.response.data) {
+                    const errorMessage = typeof error.response.data === 'string'
+                        ? JSON.parse(error.response.data).erro
+                        : error.response.data.erro;
+                    setMensagensDeErro([errorMessage]);
+                } else {
+                    setMensagensDeErro([error.message]);
+                }
+            });
     }
-  };
 
-  const increaseQuantity = (item: CartItem) => {
-    const itemIndex = findCartItemIndex(item.name);
-    if (itemIndex !== -1) {
-      const updatedCartItems = [...cartItem];
-      updatedCartItems[itemIndex].quantity += 1;
-      setCartItems(updatedCartItems);
-    }
-  };
+    const resetInputs = () => {
+        setCliente("");
+        setDesconto(0);
+        setMensagensDeErro([]);
+    };
 
-  const handleDeleteClick = (itemName: string) => {
-    const updatedCartItems = cartItem.filter((item) => item.name !== itemName);
-    setCartItems(updatedCartItems);
-  };
+    const total = cartItem.reduce((total, product) => {
+        const descontoEmReais = (desconto / 100) * (product.precoVenda * product.quantidade);
+        const subtotalComDesconto = (product.precoVenda * product.quantidade) - descontoEmReais;
+        return total + subtotalComDesconto;
+    }, 0).toFixed(2);
+    const totItens = cartItem.reduce((acc, item) => acc + item.quantidade, 0);
+    //console.log('Valores iniciais do carrinho:', cartItems);
+    return (
+        <div className="fixed inset-0 flex items-center justify-end pt-9 pb-9 ">
+            <div className="z-2 absolute inset-0 bg-gray-800 opacity-50" />
+            <div className="z-10 min-h-screen w-2/5 rounded-lg bg-white p-6">
+                <div className="flex flex-wrap">
+                    <div className="w-1/2  mb-4 pl-2">
+                        <label className="block">Cliente</label>
+                        <input
+                            type="text"
+                            value={cliente}
+                            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                                const newValue = event.target.value.replace(/[^a-zA-Z0-8 ]+/g, '');
+                                setCliente(newValue);
+                            }}
+                            className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none"
+                            required
+                        />
+                    </div>
+                    <div className="w-1/2  mb-4 pl-2">
+                        <label className="block">Desconto (%)</label>
+                        <input
+                            type="number"
+                            value={desconto}
+                            onChange={(event: React.ChangeEvent<HTMLInputElement>) => setDesconto(event.target.valueAsNumber)}
+                            className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none"
+                            required
+                        />
+                    </div>
+                </div>
+                <div className="fixed flex bottom-52 border-t-2 py-8  items-center  space-x-96">
+                </div>
+                <ul className="space-y-2 border-t-2 py-8">
+                    {cartItems.map((item) => (
+                        <li key={item.descricao} className="flex items-center justify-between">
+                            <span>
+                                <button
+                                    onClick={() => decreasequantidade(item)}
+                                    className=" text-red-400 font-bold   px-2 rounded-l-lg mr-2"
+                                >
+                                    -
+                                </button>
+                                {item.quantidade}
+                                <button
+                                    onClick={() => increasequantidade(item)}
+                                    className=" text-green-400 font-bold  px-2 rounded-r-lg ml-2 mr-2"
+                                >
+                                    +
+                                </button>
+                                {item.descricao}
+                            </span>
+                            <span>R${item.precoVenda}  x</span>
+                            <button
+                                onClick={() => onCancelItemClick(item.id)}
 
- 
+                                className="text-red-400 font-bold px-2 rounded-l-lg "
+                            >
+                                Remover
+                            </button>
+                        </li>
+                    ))}
+                </ul>
 
+                <footer className='w-{100%} inset-0 z-10'>
 
-  const total = cartItem.reduce((acc, item) => acc + item.price * item.quantity, 0);
-  const totItens = cartItem.reduce((acc, item) => acc + item.quantity, 0);
-  //console.log('Valores iniciais do carrinho:', cartItems);
-  return (
-    <div className="fixed inset-0 flex items-center justify-end pt-9 pb-9 ">
-      <div className="z-2 absolute inset-0 bg-gray-800 opacity-50" />
-      <div className="z-10 min-h-screen w-2/5 rounded-lg bg-white p-6">
-        <ul className="space-y-2">
-          {cartItems.map((item) => (
-            <li key={item.name} className="flex items-center justify-between">
-              <span>
-                <button
-                  onClick={() => decreaseQuantity(item)}
-                  className=" text-red-400 font-bold   px-2 rounded-l-lg mr-2"
-                >
-                  -
-                </button>
-                {item.quantity}
-                <button
-                  onClick={() => increaseQuantity(item)}
-                  className=" text-green-400 font-bold  px-2 rounded-r-lg ml-2 mr-2"
-                >
-                  +
-                </button>
-                {item.name}
-              </span>
-              <span>R${item.price}  x</span>
-              <button
-                onClick={() => onCancelItemClick(item.name)}
+                    <div className="fixed flex bottom-52  items-center  space-x-96">
+                        <div style={{ color: 'red' }}>
+                            {mensagensDeErro.length > 0 && (
+                                <div className="mensagens-de-erro">
+                                    {mensagensDeErro.map((mensagem, index) => (
+                                        <p key={index}>{mensagem}</p>
+                                    ))}
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                    <div className="fixed flex bottom-52 border-t-2 py-8  items-center  space-x-96">
+                        <div>
+                            <span className="text-sm ml-2 text-gray-400 font-semibold">{totItens} Itens</span>
+                        </div>
+                        <div >
+                            <span className="text-lg ml-20 text-gray-400 font-semibold">Subtotal: </span>
+                            <span className="text-lg  text-gray-400 font-semibold">R${total}</span>
+                        </div>
+                    </div>
 
-                className="text-red-400 font-bold px-2 rounded-l-lg "
-              >
-                Remover
-              </button>
-            </li>
-          ))}
-        </ul>
+                    <div className="fixed flex bottom-12 right-12 space-x-8">
+                        <div>
+                            <button
+                                className="bg-red-400 hover:bg-gray-350 px-4 py-2 rounded mr-4"
+                                onClick={onCancelAll}>
+                                Remover Todos
+                            </button>
+                        </div>
 
-        <footer className='w-{100%} inset-0 z-10'>
-          <div className="fixed flex bottom-52 border-t-2 py-8  items-center  space-x-96">
-            <div>
-              <span className="text-sm ml-2 text-gray-400 font-semibold">{totItens} Itens</span>
+                        <div>
+                            <button className="bg-gray-200 hover:bg-gray-300 px-4 py-2 rounded mr-4" onClick={onClose}>
+                                Fechar Modal
+                            </button>
+                            <button className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded" type="submit" onClick={enviarPedido}>
+                                PAGAR
+                            </button>
+                        </div>
+                    </div>
+                </footer>
             </div>
-            <div >
-              <span className="text-lg ml-20 text-gray-400 font-semibold">Subtotal: </span>
-              <span className="text-lg  text-gray-400 font-semibold">R${total}</span>
-            </div>
-          </div>
-
-          <div className="fixed flex bottom-12 right-12 space-x-8">
-            <div>
-              <button
-                className="bg-red-400 hover:bg-gray-350 px-4 py-2 rounded mr-4"
-                onClick={onCancelAll}>
-                Remover Todos
-              </button>
-            </div>
-
-            <div>
-              <button className="bg-gray-200 hover:bg-gray-300 px-4 py-2 rounded mr-4" onClick={onClose}>
-                Fechar Modal
-              </button>
-              <button className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded" type="submit">
-                PAGAR
-              </button>
-            </div>
-          </div>
-        </footer>
-      </div>
-    </div>
-  );
+        </div>
+    );
 };
 
 export default CartModal;
