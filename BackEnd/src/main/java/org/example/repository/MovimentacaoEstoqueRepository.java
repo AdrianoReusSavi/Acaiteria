c3da023dedcfca3a16f91349563bca47a3c735b2
adriano <adrianorsavi@gmail.com>
2023-11-26 21:04:12 -0300

    Comunicação Back Front consulta Balanceamento - parcial

--- BackEnd/src/main/java/org/example/repository/MovimentacaoEstoqueRepository.java
+++ BackEnd/src/main/java/org/example/repository/MovimentacaoEstoqueRepository.java
@@ -3,9 +3,14 @@ package org.example.repository;
 import org.example.enterprise.CustomQuerydslPredicateExecutor;
 import org.example.model.MovimentacaoEstoque;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface MovimentacaoEstoqueRepository extends JpaRepository<MovimentacaoEstoque, Long>, CustomQuerydslPredicateExecutor<MovimentacaoEstoque> {
-
+    @Procedure(name = "balanceamento")
+    List<Object[]> balanceamento(@Param("filter") String filter);
 }
--- BackEnd/src/main/java/org/example/service/MovimentacaoEstoqueService.java
+++ BackEnd/src/main/java/org/example/service/MovimentacaoEstoqueService.java
@@ -1,5 +1,6 @@
 package org.example.service;
 
+import org.example.model.Balanceamento;
 import org.example.model.Item;
 import org.example.model.MovimentacaoEstoque;
 import org.example.model.TipoMovimentacao;
@@ -10,9 +11,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class MovimentacaoEstoqueService {
@@ -46,6 +49,34 @@ public class MovimentacaoEstoqueService {
         MovimentacaoEstoque existingMovimentacaoEstoque = existingMovimentacaoEstoqueOptional.get();
         modelMapper.map(entity, existingMovimentacaoEstoque);
         return repository.save(existingMovimentacaoEstoque);
+    }
+
+    @Transactional
+    public List<Balanceamento> balanceamento(String filter) {
+        try {
+            if (filter == null) {
+                filter = "";
+            }
+            List<Object[]> result = repository.balanceamento(filter);
+            return result.stream()
+                    .map(this::mapToBalanceamento)
+                    .collect(Collectors.toList());
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("Erro ao gerar o relatório", e);
+        }
+    }
+
+    private Balanceamento mapToBalanceamento(Object[] row) {
+        Balanceamento balanceamento = new Balanceamento();
+        balanceamento.setData((String) row[0]);
+        balanceamento.setNomeItem((String) row[1]);
+        balanceamento.setTipo(TipoMovimentacao.valueOf((String) row[2]));
+        balanceamento.setQtdCompras((Integer) row[3]);
+        balanceamento.setQtdVendas((Integer) row[4]);
+        balanceamento.setLucroBruto((Double) row[5]);
+        balanceamento.setLucro((Double) row[6]);
+        return balanceamento;
     }
 
     public void remover(Long id) {
