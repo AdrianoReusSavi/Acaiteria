83ea6396d91159a510029ea99ba8dce98ab3b588
Jhayne Henemam <103227909+JhayneK@users.noreply.github.com>
2023-11-29 23:45:48 -0300

    Update page.tsx

--- FrontEnd/point/src/app/cashclosing-m/page.tsx
+++ FrontEnd/point/src/app/cashclosing-m/page.tsx
@@ -1,3 +1,14 @@
-export default function CashCLoM(){
-    return(<h1>ESTA NA FECHAMENTO DO CAIXA DO MES TODO</h1>)
+import MuiTableMonthly from "@/components/report/MuiTableMonthly"
+import React from 'react';
+
+    export default function CashClosingM(){
+        const DadosdaTabela = [ ];
+        const Dados = [ ];
+
+            return (
+                <div>
+                    <h1>Balanceamento</h1>
+                    <MuiTableMonthly data={DadosdaTabela} />
+                </div>
+        );
 }
